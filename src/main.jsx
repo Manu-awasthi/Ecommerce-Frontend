78f6e7b0be0eb108464c32bce13a58dae698c1c5
Manu Awasthi <manu.awasthi1508@gmail.com>
2026-01-05 19:48:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-// {1️⃣ Sign In page (form + validation)
-// 2️⃣ Create Account page
-// 3️⃣ Cart page (add/remove items)
-// 4️⃣ Convert navbar to mobile hamburger menu
-// 5️⃣ Connect cart to product cards}
